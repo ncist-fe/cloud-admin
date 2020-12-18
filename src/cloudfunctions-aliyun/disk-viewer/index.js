b513df1c45eec46473774bd786215bd708cee496
git9527 <zhangshaoneng@hotmal.com>
2020-12-18 12:49:04 +0800

    update disk-viewer function

--- src/cloudfunctions-aliyun/disk-viewer/index.js
+++ src/cloudfunctions-aliyun/disk-viewer/index.js
@@ -3,10 +3,49 @@ exports.main = async (event, context) => {
 	//event为客户端上传的参数
 	const bodyObj = JSON.parse(event.body)
   const parent = bodyObj.parent
-  console.log('query file in parent:', parent)
-	const filesInfo = await uniCloud.database().collection('opendb-netdisk-files').where({
-    parent: parent
-  }).orderBy("isFolder", "desc").orderBy("createOn","desc").get()
+  const pathArray = parent.split('/')
+  console.log('query file in parent:', pathArray)
+  const pathStack = []
+  let parentId = null
+  for (const folder of pathArray) {
+    const whereClause = {
+      name: folder,
+      parent: parentId
+    }
+    const resp = await uniCloud.database().collection('opendb-netdisk-files').where(whereClause).orderBy("isFolder", "desc").orderBy("createOn","desc").get()
+    const folderInfo = resp.data
+    if (folderInfo.length > 0) {
+      parentId = folderInfo[0]._id
+      pathStack.push({
+        name: folder,
+        id: parentId,
+        isFolder: folderInfo[0].isFolder
+      })
+    } else {
+      console.error('path not exist when query folder:', folder)
+      return {
+        code: '-1',
+        message: '路径不存在'
+      }
+    }
+  }
+  const lastPath = pathStack.slice(-1).pop()
+  let files = []
+  if (lastPath.isFolder) {
+    const whereClause = {
+      parent: lastPath.id
+    }
+    const resp = await uniCloud.database().collection('opendb-netdisk-files').where(whereClause).orderBy("isFolder", "desc").orderBy("createOn","desc").get()
+    files = resp.data
+  }
 	//返回数据给客户端
-	return filesInfo
+	return {
+    code: 0,
+    data: {
+      paths: pathStack,
+      files: files
+    }
+  }
 };
+
+
